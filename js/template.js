406a3ae50c9d2e838b91ffdd6f3fb0d5c4ad8082
cmac1212 <colin@goinstant.com>
2013-11-01 11:01:16 -0300

    fix path to stylesheets

--- js/template.js
+++ js/template.js
@@ -4,9 +4,9 @@ window.template =
     '<fieldset>' +
     '<legend>Add a stylesheet to check for conflicts</legend>' +
       '<select id="stylesheet">' +
-        '<option value="../../node_modules/widget-examples/css/vendor/bootstrap-3.0.1.min.css">Bootstrap 3.0.1</option>' +
-        '<option value="../../node_modules/widget-examples/css/vendor/bootstrap-2.3.2.min.css">Bootstrap 2.3.2</option>' +
-        '<option value="../../node_modules/widget-examples/css/vendor/jquery-ui-1.10.3.smoothness.min.css">jQuery UI 1.10.3</option>' +
+        '<option value="../node_modules/widget-examples/css/vendor/bootstrap-3.0.1.min.css">Bootstrap 3.0.1</option>' +
+        '<option value="../node_modules/widget-examples/css/vendor/bootstrap-2.3.2.min.css">Bootstrap 2.3.2</option>' +
+        '<option value="../node_modules/widget-examples/css/vendor/jquery-ui-1.10.3.smoothness.min.css">jQuery UI 1.10.3</option>' +
       '</select>' +
       '<button id="stylesheetAdd">Add Stylesheet</button>' +
     '</fieldset>' +
